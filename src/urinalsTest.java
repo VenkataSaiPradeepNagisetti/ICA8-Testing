b13313c63a16db48b4cb28cdd74c65c8ec1338c5
VenkataSaiPradeepNagisetti <112652816+VenkataSaiPradeepNagisetti@users.noreply.github.com>
2022-10-28 10:01:54 +0530

    Added Tests for getCountOfUrinals

--- src/urinalsTest.java
+++ src/urinalsTest.java
@@ -32,4 +32,41 @@ public class urinalsTest {
             urinal.goodString("XYZ");
         });
     }
+
+    @Test
+    void getCountOfUrinalsOne() {
+        System.out.println("TEST ONE getCountOfUrinals EXECUTED");
+        assertEquals(1, urinal.getCountOfUrinals("10001"));
+    }
+
+    @Test
+    void getCountOfUrinalsTwo() {
+        System.out.println("TEST TWO getCountOfUrinals EXECUTED");
+        assertEquals(0, urinal.getCountOfUrinals("1001"));
+    }
+
+    @Test
+    void getCountOfUrinalsThree() {
+        System.out.println("TEST THREE getCountOfUrinals EXECUTED");
+        assertEquals(3, urinal.getCountOfUrinals("00000"));
+    }
+
+    @Test
+    void getCountOfUrinalsFour() {
+        System.out.println("TEST FOUR getCountOfUrinals EXECUTED");
+        assertEquals(2, urinal.getCountOfUrinals("0000"));
+    }
+    @Test
+    void getCountOfUrinalsFive() {
+        System.out.println("TEST FIVE getCountOfUrinals EXECUTED");
+        assertEquals(1, urinal.getCountOfUrinals("01000"));
+    }
+
+    @Test
+    void getCountOfUrinalsSix() {
+        System.out.println("TEST SIX getCountOfUrinals EXECUTED");
+        assertEquals(-1, urinal.getCountOfUrinals("011"));
+    }
+
+
 }
